83ae1c78ce89f1795f116646d3e871ad6495d01f
Mauricio Lara Soberanis <mauricio9308@gmail.com>
2016-07-02 16:43:00 -0500

    Adding conavi pic

--- app/components/core/services/geocoder.service.js
+++ app/components/core/services/geocoder.service.js
@@ -1,3 +1,39 @@
 /**
  * Created by mauriciolara on 7/2/16.
  */
+(function(){
+    'use strict';
+
+    angular.module('myApp').factory('GeocoderService', GeocoderService);
+
+    //Injection
+    GeocoderService.$inject = ['$q'];
+
+    function GeocoderService($q){
+
+        //Public API
+        return {
+            getUserCity : getUserCity
+        };
+
+        /**
+         * Obtains the actual location of the user
+         * */
+        function getUserCity(){
+            //Creating the defer object
+            var geocoderDefer = $q.defer();
+
+            /* we request the actual location of the device*/
+            navigator.geolocation.getCurrentPosition(function( position ){
+
+            }, function( error ){
+                // In error case we still resolve to Guadalajara just in case
+                geocoderDefer.resolve('Guadalajara, Jalisco');
+            });
+
+            // Returning the promise
+            return geocoderDefer.promise;
+        }
+    }
+
+}());
